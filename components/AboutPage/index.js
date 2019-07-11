c9aca055d00fd6678f50ac6a740f5b00f369d9ba
Oleg Gromov <mail@oleggromov.com>
2019-07-12 00:02:20 +0300

    Use Helmet to manage <head>

--- components/AboutPage/index.js
+++ components/AboutPage/index.js
@@ -1,23 +1,19 @@
 import React from 'react'
 import Layout from '../Layout'
-import Header from '../Header'
 import Avatar from '../Avatar'
 import Footer from '../Footer'
 import Experience from '../Experience'
 import Text from '../Text'
-import './about.css'
 import '../base.css'
 
+
 const AboutPage = ({ data }) => {
-  return <>
-    <Layout>
-      <Header menu={data.common.menu} />
-      <Avatar />
-      <Text content={data.data.content} />
-      <Experience {...data.data.experience} />
-      <Footer {...data.common.footer} />
-    </Layout>
-  </>
+  return <Layout menu={data.common.menu} title={data.data.meta.title}>
+    <Avatar />
+    <Text content={data.data.content} />
+    <Experience {...data.data.experience} />
+    <Footer {...data.common.footer} />
+  </Layout>
 }
 
 export default AboutPage
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Header from '../Header'
+import {Helmet} from 'react-helmet'
 import './layout.css'
+import favicon from '../favicon-32.png'
 
-const Layout = ({ children }) =>
-  <div className="layout">
-    {children}
-  </div>
+const Layout = ({ children, menu, title }) =>
+  <>
+    <Helmet>
+      <title>{title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,700&display=swap&subset=cyrillic" rel="stylesheet" />
+      <link href={favicon} rel="icon" />
+    </Helmet>
 
+    <div className="layout">
+      <Header menu={menu} />
+      {children}
+    </div>
+  </>
 
 export default Layout
