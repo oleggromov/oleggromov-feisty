a75a9d2d0d44a9f41be72d2e8115607702c1a285
Oleg Gromov <mail@oleggromov.com>
2019-07-11 20:33:38 +0300

    Add Footer component

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './footer.css'
 
-const Footer = (props) =>
+const Footer = ({ copyright, author, generator }) =>
   <div className="footer">
-    Website Footer
+    <p>
+      {copyright.replace('{{NOW}}', (new Date).getFullYear())}
+      <span className="footer-author">{author}</span>
+    </p>
+    <p dangerouslySetInnerHTML={{__html: generator.replace(/<\/?p>/, '')}} />
   </div>
 
 
